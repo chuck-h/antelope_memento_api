dd825c8a06565604e6f1379fc6093a9ec8466dbb
chuck <cfharr@gmail.com>
2023-04-06 08:55:10 -0700

    Fall thru to v1 chain_api plugin

---
+++ src/modules/v1Chain/route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const chainApiPort = "8888";
+
+router.use(function (req, res, next) {
+  const v1ChainUrl = `${req.protocol}//${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
+  res.redirect(v1ChainUrl);
+})
+
+
+module.exports = router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const v1ChainRoute = require("../modules/v1Chain/route");
+// const v1HistoryRoute = require("../modules/v1History/route");
 const healthRoute = require("../modules/health/route");
 const historyRoute = require("../modules/history/route");
 const transactionRoute = require("../modules/transactionStatus/route");
 const graphqlRoute = require("../modules/graphql/route");
 
+router.use("/v1/chain/", v1ChainRoute);
 router.use("/", healthRoute);
 router.use("/", historyRoute);
 router.use("/", transactionRoute);
