819eb7f1a6ddb0448345a180e69fe5098487dfb6
chuck <cfharr@gmail.com>
2023-04-06 09:02:21 -0700

    fix v1ChainUrl

--- src/modules/v1Chain/route.js
+++ src/modules/v1Chain/route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const chainApiPort = "8888";
 
 router.use(function (req, res, next) {
-  const v1ChainUrl = `${req.protocol}//${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
+  const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
   res.redirect(v1ChainUrl);
 })
 
