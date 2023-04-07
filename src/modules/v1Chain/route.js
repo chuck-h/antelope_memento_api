083e8917c267f35a6e20db30bc2d7623bdb788f0
chuck <cfharr@gmail.com>
2023-04-06 21:30:05 -0700

    router testing

--- src/modules/v1Chain/route.js
+++ src/modules/v1Chain/route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const chainApiPort = "8888";
 
-router.post('/', function (req, res, next) {
+router.post('/v1/chain/', function (req, res, next) {
   const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
   res.redirect(307, v1ChainUrl);
 })
