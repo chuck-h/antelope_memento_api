c8d6d528fce50549b1ff2b545a6afeed2f0b41f8
chuck <cfharr@gmail.com>
2023-04-07 08:24:22 -0700

    rt

--- src/modules/v1Chain/route.js
+++ src/modules/v1Chain/route.js
@@ -7,15 +7,10 @@ const chainApiPort = "8888";
 
 router.use('/', function (req, res, next) {
   const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
-  console.log(v1ChainUrl);
+  console.log(`v1Chain: ${req.method} ${v1ChainUrl}`);
   return proxy(v1ChainUrl, {filter: function(req, res) {
     return req.method == 'POST'}
   })(req, res, next);
-},)
-router.get('/get_info', function (req, res, next) {
-  const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
-  return proxy(v1ChainUrl)(req, res, next);
 })
-
 
 module.exports = router;
