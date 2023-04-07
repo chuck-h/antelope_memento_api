2da2757cf0563de05e85e5c306cfc78fc970caed
chuck <cfharr@gmail.com>
2023-04-07 08:53:10 -0700

    rt

--- src/modules/v1Chain/route.js
+++ src/modules/v1Chain/route.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const chainApiPort = "8888";
 
 router.use('/', function (req, res, next) {
-  const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
+  const v1ChainUrl = `${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
+  //  const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
   console.log(`v1Chain: ${req.method} ${v1ChainUrl}`);
   
   return proxy(v1ChainUrl, {preserveHostHdr: true})(req, res, next);
