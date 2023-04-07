9c67faeeac0835f6b74758a211244946c8bec5a5
chuck <cfharr@gmail.com>
2023-04-06 21:31:53 -0700

    router testing

--- src/modules/v1Chain/route.js
+++ src/modules/v1Chain/route.js
@@ -8,7 +8,7 @@ router.post('/v1/chain/', function (req, res, next) {
   const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
   res.redirect(307, v1ChainUrl);
 })
-router.get('/'. function (req, res, next) {
+router.get('/', function (req, res, next) {
   const v1ChainUrl = `${req.protocol}://${req.hostname}:${chainApiPort}${req.baseUrl}${req.path}`;
   res.redirect(307, v1ChainUrl);
 })
